9689c554c7d79c5d2ed47447bf08c28ef3b50eac
Joseph <21026773+jlopgit@users.noreply.github.com>
2018-10-04 21:07:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+
+      function showAteIn(){
+
+        document.getElementById('ateIn').style.display = 'block';
+        document.getElementById('ateOut').style.display = 'none';
+      }
+
+      function showAteOut(){
+
+        document.getElementById('ateOut').style.display = 'block';
+        document.getElementById('ateIn').style.display = 'none';
+      }
+
+      function clearForms(){
+
+          document.getElementById('ateIn').reset();
+          document.getElementById('ateOut').reset();
+      }
+
+      function newAteInEntry(){
+
+        var obj = {
+
+          type: document.getElementById('ateIn').name,
+          dish: document.getElementById('dishIn').value,
+          time: document.getElementById('time').value,
+          ingredients: document.getElementById('ingredients').value,
+          cost: document.getElementById('cost').value,
+          instructions: document.getElementById('instructions').value
+        }
+
+        getFood(obj);
+      }
+
+      function newAteOutEntry(){
+
+        var obj = {
+
+          type: document.getElementById('ateOut').name,
+          restaurant: document.getElementById('restaurant').value,
+          location: document.getElementById('location').value,
+          price: document.getElementById('price').value,
+          dish: document.getElementById('dishOut').value,
+          description: document.getElementById('description').value
+        }
+
+        getFood(obj);
+      }
+
+      function getFood(foodObj){
+
+        /*var i = 0;
+        var text = "";
+
+        for(i in foodObj){
+          text += foodObj[i] + " ";
+        }
+
+        alert(text);*/
+
+        var myJSON = JSON.stringify(foodObj);
+
+        alert(myJSON);
+
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.open("POST", "/create_food", true);
+        //used application/json because i'm sending json.
+        //otherwise use x-www-form-urlencoded
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.send(myJSON);
+      }
+
+      /*
+      var dish;
+      var time;
+      var ingredients;
+      var description;
+      var price;
+      var location;
+      var photo;
+
+      var formInput = [];
+      var formNodes = [];
+
+      function newAteInEntry(){
+
+        //photo = document.getElementById('photos').value;
+        dish = document.getElementById('dishIn').value;
+        time = document.getElementById('time').value;
+        ingredients = document.getElementById('ingredients').value;
+        price = document.getElementById('cost').value;
+        description = document.getElementById('instructions').value;
+
+        var dishContent = document.createTextNode(dish);
+        var timeContent = document.createTextNode(time);
+        var ingredientsContent = document.createTextNode(ingredients);
+        var priceContent = document.createTextNode(price);
+        var descriptionContent = document.createTextNode(description);
+
+        var dishNode = document.createElement("p");
+        var timeNode = document.createElement("p");
+        var ingredientsNode = document.createElement("p");
+        var priceNode = document.createElement("p");
+        var descriptionNode = document.createElement("p");
+
+        dishNode.appendChild(dishContent);
+        timeNode.appendChild(timeContent);
+        ingredientsNode.appendChild(ingredientsContent);
+        priceNode.appendChild(priceContent);
+        descriptionNode.appendChild(descriptionContent);
+
+        var node = document.createElement("div");
+        node.className = "jumbotron";
+        node.appendChild(dishNode);
+        node.appendChild(timeNode);
+        node.appendChild(ingredientsNode);
+        node.appendChild(priceNode);
+        node.appendChild(descriptionNode);
+        document.getElementById("entries").prepend(node);
+
+        dish, time, ingredients, price, description = "";
+      }
+      */
+
+
+      /*
+      getFood(){
+
+        var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              var javaObj = JSON.parse(this.responseText);
+              document.getElementById("entries").innerHTML = javaObj.id;
+            }
+          };
+          xhttp.open("GET", "food.json", true);
+          xhttp.send();
+      }
+      */
--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var dbURL = "mongodb://localhost:27017/";
 
 var app = express();
 
-//middle to be able to parse form info.
-app.use(bodyParser.urlencoded({extended: false}));
+//middleware to be able to parse form info.
+app.use(express.urlencoded({extended: false}));
+//middleware to parse json
+app.use(express.json());
+
 
 //serve all static pages.
 app.use(express.static('./'));
@@ -21,23 +24,22 @@ app.get("/", function(req,res){
 });
 
 app.post("/create_food", function(req, res){
+//somehow json sent from the body is converted into a javascript Obj.
 
 	console.log("post req received");
 
-	
-	//console.log(req.body.ateInDish + ", " + req.body.ateInInstructions);
+	console.log(req.body);
 
-	/*
 	MongoClient.connect(dbURL, function(err, db){
 
 		if(err) throw err;
 
-		var dbInput = {dish: req.body.ateInDish, instrucion: req.body.ateInInstructions}
+		//var dbInput = {{dish: req.body.ateInDish, instrucion: req.body.ateInInstructions}}
 
 		//insert to database
 		var dbObj = db.db("foodie");
 
-		dbObj.collection("food").insertOne(dbInput, function(err, res){
+		dbObj.collection("food").insertOne(req.body, function(err, res){
 
 			if(err) throw err;
 
@@ -45,10 +47,9 @@ app.post("/create_food", function(req, res){
 			db.close();
 		});
 	});
-	*/
+
 
 	//console log all database contents
-	/*
 	MongoClient.connect(dbURL, function(err, db) {
 		if (err) throw err;
 
@@ -61,37 +62,10 @@ app.post("/create_food", function(req, res){
 		});
 
 	});
-	*/
+
 
 	res.end();
 });
 
 
-
-
-
 app.listen(8080);
-
-
-/*
-http.createServer(function(req, res){
-
-
-	var q = url.parse(req.url, true);
-	var filename = "." + q.pathname;
-	var qdata = q.query;
-
-	fs.readFile(filename, function(err, data) {
-
-		if(err){
-
-			res.writeHead(404, {'Content-Type' : 'text/html'});
-			return res.end("404 Not Found");
-		}
-
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		return res.end();
-  });
-}).listen(8080);
-*/
